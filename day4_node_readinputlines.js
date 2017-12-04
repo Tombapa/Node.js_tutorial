a681473e15c674d503a3d87441fc9c1d34718397
Tombapa <tommi.paavilainen@gmail.com>
2017-12-04 22:17:58 +0200

    Tutorial playground.

---
+++ day4_node_readinputlines.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+
+function readLines(input, func) {
+    var remaining = '';
+  
+    input.on('data', function(data) {
+      remaining += data;
+      var index = remaining.indexOf('\n');
+      var last  = 0;
+      while (index > -1) {
+        var line = remaining.substring(last, index);
+        last = index + 1;
+        func(line);
+        index = remaining.indexOf('\n', last);
+      }
+  
+      remaining = remaining.substring(last);
+    });
+  
+    input.on('end', function() {
+      if (remaining.length > 0) {
+        func(remaining);
+      }
+    });
+  }
+
+function func(data) {
+  console.log('Line: ' + data);
+}
+
+var input = fs.createReadStream('attachments\\day4input.txt');
+readLines(input, func);
